72101135de9c3e64ec2519d08e9f51b58797f8b9
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-01 15:08:10 +0300

    Task019 done

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. 
+// Не использовать String!
+
+int num = UserInput();
+
+int rev = CheckPalindrome(num);
+
+PrintAnswer(rev);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int UserInput()
+{
+    Console.WriteLine("Введите любое пятизначное число:");
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+    // if(isNum && num > 9999 && num < 100000)
+    if(isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+     
+}
+
+int CheckPalindrome(int num)
+{
+    int n = num;
+    int rev = 0;
+    int result;
+    while(n > 0)
+    {
+        int r = n % 10;
+        rev = rev * 10 + r;
+        n = n / 10;
+    }
+    return rev;
+}
+
+int PrintAnswer(int rev)
+{
+    if(rev == num)
+    {
+        Console.WriteLine($"Введенное Вами число является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine($"Введенное Вами число не является палиндромом.");
+    }
+    return -1;
+}
