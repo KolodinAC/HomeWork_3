cea6cb74a6fba7a56c6210442633961e545ddb07
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-01 15:26:23 +0300

    fix

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. 
+// Задание должно быть выполнено в методе. 
+// В методе не должно быть вывода в консоль.
+
+int UserInput()
+{
+    Console.WriteLine("Введите координаты заданных точек:");
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+    if(isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректные значения");
+        return -1;
+    }
+     
+}
+
+
+int x1 = UserInput();
+int y1 = UserInput();
+int z1 = UserInput();
+int x2 = UserInput();
+int y2 = UserInput();
+int z2 = UserInput();
+int x3 = UserInput();
+int y3 = UserInput();
+int z3 = UserInput();
