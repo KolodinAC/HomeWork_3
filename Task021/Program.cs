dbbe34c66312b85a1965fa7975cfc9fbce112aa9
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-01 15:52:04 +0300

    Task021 done

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -2,6 +2,51 @@
 // Задание должно быть выполнено в методе. 
 // В методе не должно быть вывода в консоль.
 
+int x1 = UserInput();
+int y1 = UserInput();
+int z1 = UserInput();
+int x2 = UserInput();
+int y2 = UserInput();
+int z2 = UserInput();
+
+double result = GetDistanceBetweenTwoPoints(x1, y1, z1, x2, y2, z2); 
+
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int UserInput()
 {
     Console.WriteLine("Введите координаты заданных точек:");
@@ -18,13 +63,7 @@ int UserInput()
      
 }
 
-
-int x1 = UserInput();
-int y1 = UserInput();
-int z1 = UserInput();
-int x2 = UserInput();
-int y2 = UserInput();
-int z2 = UserInput();
-int x3 = UserInput();
-int y3 = UserInput();
-int z3 = UserInput();
+double GetDistanceBetweenTwoPoints(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2)), 2);
+}
